4d0bd489d10dc70d3782a4a1cae989eeca1d09cb
Colin Graber <colin.graber@gmail.com>
2013-09-21 04:18:51 -0400

    Created rails directory structure

---
+++ YourStory/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YourStory::Application.config.secret_key_base = 'fd15eaf3f463c91e222910704589d0e0817d82cd6da2d619a7291b8483c2e09811fb4b984529e251e1188575facd295ad713698bc46a1f74684d6f6716a75ef5'
---
+++ YourStory/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+YourStory::Application.config.session_store :cookie_store, key: '_YourStory_session'
