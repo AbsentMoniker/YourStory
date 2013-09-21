63d2b0ae374e735f095cd929820df44340c84ef6
bai15 <bai15@purdue.edu>
2013-09-21 19:34:45 -0400

    psd, model

--- YourStory/config/routes.rb
+++ YourStory/config/routes.rb
@@ -1,69 +1,3 @@
-<<<<<<< HEAD
-YourStory::Application.routes.draw do
-  get "users/new"
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-  
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-  get "home/index"
-  resources :users
-  resources :sessions, only: [:new, :create, :destroy]
-  root 'home#index'
-  match '/signup', to: 'users#new', via: 'get'
-  match '/signin', to: 'sessions#new', via: 'get' 
-  match '/signout', to: 'sessions#destroy', via: 'delete'
-end
-=======
 YourStory::Application.routes.draw do
   
   # The priority is based upon order of creation: first created -> highest priority.
@@ -131,4 +65,3 @@ YourStory::Application.routes.draw do
   match '/:name', to: 'users#show', via: 'get'
   
 end
->>>>>>> 2810bff28fb93e1f2510d8694a755f970046d85c
