0e65422c22a0fd8d1bb8067c8a0fffe2d360d2cc
Colin Graber <colin.graber@gmail.com>
2013-09-21 20:33:16 -0400

    Fixed pluralization of story

--- YourStory/config/routes.rb
+++ YourStory/config/routes.rb
@@ -1,5 +1,7 @@
 YourStory::Application.routes.draw do
   
+  get "storys/new"
+  get "storys/create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
